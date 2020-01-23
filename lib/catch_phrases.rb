745cc84f4b24f20b8ce0357c2612bb6a17357267
spectacular-system-6224 <d.leslie707@gmail.com>
2020-01-23 08:44:21 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,12 +1,22 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
+  phrase = "BRO"
   puts phrase
 end
 
 def toadstool
+   status = "We are super valid"
   puts status
 end
 
 def link
   puts "It's not cool to go alone! Take this"
 end
+
+def any_phrase
+  name_mario = mario
+  name_toadstool = toadstool
+  name_link = link
+end
+
+any_phrase
